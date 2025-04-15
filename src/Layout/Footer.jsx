33bb30d0ad7983f3b19f3417ea726324f94645e3
Adnan Ashraf <ashrafadnan959@gmail.com>
2025-04-15 15:12:44 +0530

    Update Footer.jsx

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="bg-black text-white ">
-        <p className="flex h-[10vh] items-center justify-center">All rights reserved &copy; Adnan2k5</p>
+        <p className="flex h-[10vh] items-center justify-center">All rights reserved</p>
     </div>
   )
 }
